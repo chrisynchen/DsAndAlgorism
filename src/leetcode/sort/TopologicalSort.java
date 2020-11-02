6fed94bcbc1ce346116635f9b9d0ea6651e45d8d
Chris Chen <chris.chen@grabtaxi.com>
2020-11-02 11:55:29 +0800

    TopologicalSort

---
+++ src/leetcode/sort/TopologicalSort.java
@@ -0,0 +1,73 @@
+package leetcode.sort;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * [5, 2], [5,0], [4,0], [4,1], [2,3], [3,1]
+ */
+public class TopologicalSort {
+    public static void main(String[] args) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] tasks = new int[]{0, 1, 2, 3, 4, 5};
+
+        for (int i = 0; i < tasks.length; ++i)
+            adj.add(new ArrayList<>());
+
+        addEdge(adj, 5, 2);
+        addEdge(adj, 5, 0);
+        addEdge(adj, 4, 0);
+        addEdge(adj, 4, 1);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 3, 1);
+
+        topologicalSort(adj, 6);
+    }
+
+    private static void addEdge(List<List<Integer>> adj, int v, int w) {
+        adj.get(v).add(w);
+    }
+
+    static void topologicalSortUtil(List<List<Integer>> adj, int v, boolean visited[],
+                                    Stack<Integer> stack) {
+        // Mark the current node as visited.
+        visited[v] = true;
+        Integer i;
+
+        // Recur for all the vertices adjacent
+        // to thisvertex
+        Iterator<Integer> it = adj.get(v).iterator();
+        while (it.hasNext()) {
+            i = it.next();
+            if (!visited[i])
+                topologicalSortUtil(adj, i, visited, stack);
+        }
+
+        // Push current vertex to stack
+        // which stores result
+        stack.push(new Integer(v));
+    }
+
+    // The function to do Topological Sort.
+    // It uses recursive topologicalSortUtil()
+    static void topologicalSort(List<List<Integer>> adj, int V) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        // Mark all the vertices as not visited
+        boolean visited[] = new boolean[V];
+
+        // Call the recursive helper
+        // function to store
+        // Topological Sort starting
+        // from all vertices one by one
+        for (int i = 0; i < V; i++)
+            if (visited[i] == false)
+                topologicalSortUtil(adj, i, visited, stack);
+
+        // Print contents of stack
+        while (stack.empty() == false)
+            System.out.print(stack.pop() + " ");
+    }
+}
