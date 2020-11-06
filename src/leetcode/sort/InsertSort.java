683208f5e6222564c3a55b06440d5ae44a64ddb9
Chris Chen <chris.chen@grabtaxi.com>
2020-11-07 01:19:58 +0800

    add insert sort for linked list

--- src/leetcode/sort/InsertSort.java
+++ src/leetcode/sort/InsertSort.java
@@ -12,7 +12,7 @@ public class InsertSort {
     private static void insertSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
-                if(nums[i] < nums[j]) {
+                if (nums[i] < nums[j]) {
                     swap(nums, i, j);
                 }
             }
@@ -23,5 +23,47 @@ public class InsertSort {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    public static ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode helper = new ListNode(0);
+        ListNode prevNode = helper;
+        ListNode currentNode = head;
+        ListNode nextNode = null;
+
+        while (currentNode != null) {
+            nextNode = currentNode.next;
+
+            while (prevNode.next != null && prevNode.next.val < currentNode.val) {
+                prevNode = prevNode.next;
+            }
+            //insert
+            currentNode.next = prevNode.next;
+            prevNode.next = currentNode;
+
+            prevNode = helper;
+            currentNode = nextNode;
+        }
+
+        return helper.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
